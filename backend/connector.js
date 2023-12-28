09db2cc497b0f895d3a19e63d0875ff66c9cdde1
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2023-12-28 15:37:35 +0530

    env update

--- backend/connector.js
+++ backend/connector.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose');
 const mongodb = require('mongodb');
-mongoose.set("strictQuery", true);
+
 
 //local mongo url : mongodb://localhost:27017/
-const mongoURI = process.env.MONGODB_URL + 'bookMovie'
+const mongoURI = `mongodb+srv://ankushrana9458:${process.env.MONGODB_PWD}@cluster0.38osesy.mongodb.net/?retryWrites=true&w=majority` + "bookMovie"
 
 
 const { bookMovieSchema } = require('./schema')
@@ -18,3 +18,4 @@ let collection_connection = mongoose.model('bookmovietickets', bookMovieSchema)
 
 
 exports.connection = collection_connection;
+
