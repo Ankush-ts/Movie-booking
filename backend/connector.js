c3383dc5c2d61c654ffcc1ab0db4b818bc007757
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2023-12-28 13:38:49 +0530

    update

--- backend/connector.js
+++ backend/connector.js
@@ -1,9 +1,11 @@
+const mongoose = require('mongoose');
 const mongodb = require('mongodb');
+mongoose.set("strictQuery", true);
 
 //local mongo url : mongodb://localhost:27017/
 const mongoURI = process.env.MONGODB_URL + 'bookMovie'
 
-let mongoose = require('mongoose');
+
 const { bookMovieSchema } = require('./schema')
 
 
