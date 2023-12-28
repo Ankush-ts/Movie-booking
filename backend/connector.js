ab879666463bb23b918bd0d877151ef72d75ca9d
Ankush-ts <132140570+Ankush-ts@users.noreply.github.com>
2023-12-28 12:30:53 +0530

    Unified removed from mongoose connect

--- backend/connector.js
+++ backend/connector.js
@@ -7,7 +7,7 @@ let mongoose = require('mongoose');
 const { bookMovieSchema } = require('./schema')
 
 
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoURI)
     .then(() => { console.log("connection established with mongodb server online"); })
     .catch(err => {
         console.log("error while connection", err)
